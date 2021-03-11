8386ba55003b4d44c20049860eb7ca9436a4f6dd
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-11 17:25:52 +0700

    Google Sign-In

--- FirebaseCourceByPhillipLackner/app/src/main/java/com/example/firebasecourcebyphilliplackner/MainActivity.kt
+++ FirebaseCourceByPhillipLackner/app/src/main/java/com/example/firebasecourcebyphilliplackner/MainActivity.kt
@@ -1,17 +1,22 @@
 package com.example.firebasecourcebyphilliplackner
 
-import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.UserProfileChangeRequest
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+
+const val REQUEST_CODE_SIGN_IN = 0
 
 class MainActivity : AppCompatActivity() {
     lateinit var auth: FirebaseAuth
@@ -20,101 +25,44 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         auth = FirebaseAuth.getInstance()
-        auth.signOut()
 
-        btnRegister.setOnClickListener {
-            registerUser()
-        }
-
-        btnLogin.setOnClickListener {
-            loginUser()
-        }
-
-        btnUpdateProfile.setOnClickListener {
-            updateProfile()
-        }
-    }
-
-    private fun updateProfile() {
-        auth.currentUser?.let { user ->
-            val username = etUsername.text.toString()
-            val photoURI = Uri.parse("android.resource://$packageName/${R.drawable.picture_anime}")
-            val profileUpdate = UserProfileChangeRequest.Builder()
-                    .setDisplayName(username)
-                    .setPhotoUri(photoURI)
+        btnGoogleSignIn.setOnClickListener {
+            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                    .requestIdToken(getString(R.string.webclient_id))
+                    .requestEmail()
                     .build()
 
+            val signInClient = GoogleSignIn.getClient(this, options)
+            signInClient.signInIntent.also {
+                startActivityForResult(it, REQUEST_CODE_SIGN_IN) //const val
+            }
+        }
+    }
 
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    user.updateProfile(profileUpdate).await()
-                    checkLoggedInState()
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, "Successfully update user profile",
-                                Toast.LENGTH_LONG).show()
-                    }
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
+    private fun googleAuthForFireBase(account: GoogleSignInAccount) {
+        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
+        CoroutineScope(Dispatchers.IO).launch {
+            try {
+                auth.signInWithCredential(credentials).await()
+                withContext(Dispatchers.Main){
+                    Toast.makeText(this@MainActivity, "Succesfully logged in", Toast.LENGTH_LONG).show()
+                }
+
+            } catch (e: Exception) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
                 }
             }
         }
     }
 
-
-    override fun onStart() { //Mengecek apakah user sudah login
-        super.onStart()
-        checkLoggedInState()
-    }
-
-    private fun registerUser() {
-        val email = etEmailRegister.text.toString()
-        val password = etPasswordRegister.text.toString()
-        if (email.isNotEmpty() && password.isNotEmpty()) {
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.createUserWithEmailAndPassword(email, password).await() // Mengambil data email dan password yang sudah di isi
-                    withContext(Dispatchers.Main) {// Mengecek apakah akun sudah terdaftar
-                        checkLoggedInState()
-                    }
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_SIGN_IN) {
+            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
+            account?.let {
+                googleAuthForFireBase(it)
             }
-        }
-    }
-
-
-    private fun loginUser() {
-        val email = etEmailLogin.text.toString()
-        val password = etPasswordLogin.text.toString()
-        if (email.isNotEmpty() && password.isNotEmpty()) {
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.signInWithEmailAndPassword(email, password).await() // Mengambil data email dan password yang sudah di isi
-                    withContext(Dispatchers.Main) {// Mengecek apakah akun sudah terdaftar
-                        checkLoggedInState()
-                    }
-                } catch (e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun checkLoggedInState() {
-        val user = auth.currentUser
-        if (user == null) {
-            tvLoggedIn.text = "You are not logged in"
-        } else {
-            tvLoggedIn.text = "You are logged in"
-            etUsername.setText(user.displayName)
-            ivProfilePicture.setImageURI(user.photoUrl)
         }
     }
 }
