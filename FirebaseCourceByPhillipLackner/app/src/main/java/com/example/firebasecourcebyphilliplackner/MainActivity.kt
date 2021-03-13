a715692c66f241583360400d6b5ecedb5ff41e93
Kevin Williams Permana <kevinity310@gmail.com>
2021-03-13 22:39:26 +0700

    Uploading Data

--- FirebaseCourceByPhillipLackner/app/src/main/java/com/example/firebasecourcebyphilliplackner/MainActivity.kt
+++ FirebaseCourceByPhillipLackner/app/src/main/java/com/example/firebasecourcebyphilliplackner/MainActivity.kt
@@ -1,14 +1,10 @@
 package com.example.firebasecourcebyphilliplackner
 
-import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -16,52 +12,32 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-const val REQUEST_CODE_SIGN_IN = 0
-
 class MainActivity : AppCompatActivity() {
-    lateinit var auth: FirebaseAuth
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        auth = FirebaseAuth.getInstance()
 
-        btnGoogleSignIn.setOnClickListener {
-            val options = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                    .requestIdToken(getString(R.string.webclient_id))
-                    .requestEmail()
-                    .build()
-
-            val signInClient = GoogleSignIn.getClient(this, options)
-            signInClient.signInIntent.also {
-                startActivityForResult(it, REQUEST_CODE_SIGN_IN) //const val
-            }
+        btnSendToDatabase.setOnClickListener {
+            val firstName = etFirstName.text.toString()
+            val lastName = etLastName.text.toString()
+            val age = etAge.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
         }
     }
 
-    private fun googleAuthForFireBase(account: GoogleSignInAccount) {
-        val credentials = GoogleAuthProvider.getCredential(account.idToken, null)
-        CoroutineScope(Dispatchers.IO).launch {
-            try {
-                auth.signInWithCredential(credentials).await()
-                withContext(Dispatchers.Main){
-                    Toast.makeText(this@MainActivity, "Succesfully logged in", Toast.LENGTH_LONG).show()
-                }
-
-            } catch (e: Exception) {
-                withContext(Dispatchers.Main) {
-                    Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
-                }
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, "Successfully save data", Toast.LENGTH_LONG).show()
             }
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (requestCode == REQUEST_CODE_SIGN_IN) {
-            val account = GoogleSignIn.getSignedInAccountFromIntent(data).result
-            account?.let {
-                googleAuthForFireBase(it)
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_LONG).show()
             }
         }
     }
---
+++ FirebaseCourceByPhillipLackner/app/src/main/java/com/example/firebasecourcebyphilliplackner/Person.kt
@@ -0,0 +1,7 @@
+package com.example.firebasecourcebyphilliplackner
+
+data class Person(
+    val firstName: String,
+    val lastName: String,
+    val age: Int
+)
